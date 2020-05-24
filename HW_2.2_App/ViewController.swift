83a24055f2a4e5a608b8ce2dcccb3298781e9e90
Aleksei Podlevskii <alexp999@mail.ru>
2020-05-24 21:32:18 +0500

    Initial Commit

---
+++ HW_2.2_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_2.2_App
+//
+//  Created by Алексей Подлевский on 24.05.2020.
+//  Copyright © 2020 Independent Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
